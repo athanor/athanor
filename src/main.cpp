929970fca7ddca8a340e985fb84f9edd2888dc42
saad_attieh <saad_attieh@me.com>
2017-10-01 06:20:31 +0100

    fixed hashing bug, all working now

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,18 @@
 #include <iostream>
+#include "common/common.h"
 #include "types/allTypes.h"
 #include "types/forwardDecls/changeValue.h"
+#include "types/forwardDecls/hash.h"
 #include "types/forwardDecls/print.h"
+#include "utils/hashUtils.h"
 using namespace std;
 
 int main() {
-    SetDomain s(exactSize(4),
-                SetDomain(maxSize(1), IntDomain({intBound(1, 3)})));
+    SetDomain s(noSize(), SetDomain(noSize(), IntDomain({intBound(1, 3)})));
     cout << "Building values with domain: ";
     prettyPrint(cout, s) << endl;
     auto v = makeInitialValueInDomain(s);
+
     do {
         prettyPrint(cout, *v) << endl;
     } while (moveToNextValueInDomain(*v, s, []() { return true; }));
--- src/utils/hashUtils.h
+++ src/utils/hashUtils.h
@@ -12,14 +12,15 @@ inline uint64_t fnv1a(unsigned char oneByte, uint64_t hash = FNV1A_SEED) {
 template <typename T>
 inline u_int64_t mix_single(const T& value, uint64_t hash = FNV1A_SEED) {
     const unsigned char* ptr = (const unsigned char*)&value;
-    for (int i = 0; i < sizeof(T); ++i) {
+    for (size_t i = 0; i < sizeof(T); ++i) {
         hash = fnv1a(*ptr++, hash);
     }
     return hash;
 }
 
 template <typename... Types>
-inline u_int64_t mix(const Types&... args, uint64_t hash = FNV1A_SEED) {
+inline u_int64_t mix(const Types&... args) {
+    uint64_t hash = FNV1A_SEED;
     // hack to apply mix_single to all types
 
     int unpack[]{0, (hash = mix_single(args, hash), 0)...};

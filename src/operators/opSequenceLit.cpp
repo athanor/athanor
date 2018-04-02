671af5eeb227b600c2351e021ffad79c4d8360f0
saad_attieh <saad_attieh@me.com>
2018-04-02 14:01:16 +0100

    added op sequence lit

---
+++ src/operators/opSequenceLit.cpp
@@ -0,0 +1,120 @@
+#include "operators/opSequenceLit.h"
+#include <cassert>
+#include <cstdlib>
+#include "types/allTypes.h"
+#include "utils/ignoreUnused.h"
+using namespace std;
+void OpSequenceLit::evaluate() {
+    mpark::visit(
+        [&](auto& members) {
+            for (auto& member : members) {
+                member->evaluate();
+            }
+        },
+        members);
+}
+template <typename TriggerType>
+struct ExprTrigger
+    : public OpSequenceLit::ExprTriggerBase,
+      public ChangeTriggerAdapter<TriggerType, ExprTrigger<TriggerType>> {
+    using ExprTriggerBase::ExprTriggerBase;
+    void adapterPossibleValueChange() {
+        mpark::visit(
+            [&](auto& members) {
+                this->op->template notifyPossibleSubsequenceChange<viewType(
+                    members)>(this->index, this->index + 1);
+            },
+            this->op->members);
+    }
+    void adapterValueChanged() {
+        mpark::visit(
+            [&](auto& members) {
+                this->op
+                    ->template changeSubsequenceAndNotify<viewType(members)>(
+                        this->index, this->index + 1);
+            },
+            this->op->members);
+    }
+};
+
+OpSequenceLit::OpSequenceLit(OpSequenceLit&& other)
+    : SequenceView(std::move(other)),
+      exprTriggers(std::move(other.exprTriggers)) {
+    setTriggerParent(this, exprTriggers);
+}
+
+void OpSequenceLit::startTriggering() {
+    if (exprTriggers.empty()) {
+        mpark::visit(
+            [&](auto& members) {
+                typedef typename AssociatedTriggerType<viewType(members)>::type
+                    TriggerType;
+                for (size_t i = 0; i < members.size(); i++) {
+                    auto trigger =
+                        make_shared<ExprTrigger<TriggerType>>(this, i);
+                    addTrigger(members[i], trigger);
+                    exprTriggers.emplace_back(move(trigger));
+                    members[i]->startTriggering();
+                }
+            },
+            members);
+    }
+}
+
+void OpSequenceLit::stopTriggering() {
+    if (!exprTriggers.empty()) {
+        mpark::visit(
+            [&](auto& members) {
+                typedef typename AssociatedTriggerType<viewType(members)>::type
+                    TriggerType;
+                for (auto& member : members) {
+                    member->stopTriggering();
+                }
+                for (auto& trigger : exprTriggers) {
+                    deleteTrigger(
+                        static_pointer_cast<ExprTrigger<TriggerType>>(trigger));
+                }
+                exprTriggers.clear();
+            },
+            members);
+    }
+}
+
+void OpSequenceLit::updateViolationDescription(UInt, ViolationDescription&) {}
+
+ExprRef<SequenceView> OpSequenceLit::deepCopySelfForUnroll(
+    const AnyIterRef& iterator) const {
+    AnyExprVec newMembers;
+    mpark::visit(
+        [&](auto& members) {
+            auto& newMembersImpl =
+                newMembers.emplace<ExprRefVec<viewType(members)>>();
+            for (auto& member : members) {
+                newMembersImpl.emplace_back(
+                    deepCopyForUnroll(member, iterator));
+            }
+        },
+        members);
+
+    auto newOpSequenceLit = make_shared<OpSequenceLit>(move(newMembers));
+    return ViewRef<SequenceView>(newOpSequenceLit);
+}
+
+std::ostream& OpSequenceLit::dumpState(std::ostream& os) const {
+    os << "OpSequenceLit: exprs=[";
+    mpark::visit(
+        [&](auto& members) {
+            bool first = true;
+            for (const auto& member : members) {
+                if (first) {
+                    first = false;
+                } else {
+                    os << ",";
+                }
+                member->dumpState(os);
+            }
+        },
+        members);
+    os << "]";
+    return os;
+}
---
+++ src/operators/opSequenceLit.h
@@ -0,0 +1,28 @@
+#ifndef SRC_OPERATORS_OPSEQUENCELIT_H_
+#define SRC_OPERATORS_OPSEQUENCELIT_H_
+#include "base/base.h"
+#include "types/sequence.h"
+
+struct OpSequenceLit : public SequenceView {
+    struct ExprTriggerBase {
+        OpSequenceLit* op;
+        UInt index;
+        ExprTriggerBase(OpSequenceLit* op, UInt index) : op(op), index(index) {}
+    };
+    std::vector<std::shared_ptr<ExprTriggerBase>> exprTriggers;
+
+    OpSequenceLit(AnyExprVec members) : SequenceView(std::move(members)) {}
+    OpSequenceLit(const OpSequenceLit&) = delete;
+    OpSequenceLit(OpSequenceLit&& other);
+    ~OpSequenceLit() { this->stopTriggering(); }
+    void evaluate() final;
+    void startTriggering() final;
+    void stopTriggering() final;
+    void updateViolationDescription(UInt parentViolation,
+                                    ViolationDescription&) final;
+    ExprRef<SequenceView> deepCopySelfForUnroll(
+        const AnyIterRef& iterator) const final;
+    std::ostream& dumpState(std::ostream& os) const final;
+};
+
+#endif /* SRC_OPERATORS_OPSEQUENCELIT_H_ */

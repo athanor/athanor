18646d68a0671fba4df9c5689bb44dfa3694fb2d
saad_attieh <saad_attieh@me.com>
2018-04-27 19:03:49 +0100

    op sequence index still not working, compiling on specific versions of gcc

--- src/base/exprRef.cpp
+++ src/base/exprRef.cpp
@@ -1,4 +1,5 @@
 #include "base/exprRef.h"
+#include <memory>
 #include "base/valRef.h"
 #include "types/allTypes.h"
 #include "utils/ignoreUnused.h"
@@ -32,13 +33,13 @@ ostream& prettyPrint(ostream& os, const AnyExprRef& expr) {
 template <typename Value>
 ExprRef<typename AssociatedViewType<Value>::type> ValRef<Value>::asExpr() {
     typedef typename AssociatedViewType<Value>::type View;
-    return ExprRef<View>(static_pointer_cast<View>(this->getPtr()));
+    return static_pointer_cast<View>(this->getPtr());
 }
 template <typename Value>
 const ExprRef<typename AssociatedViewType<Value>::type> ValRef<Value>::asExpr()
     const {
     typedef typename AssociatedViewType<Value>::type View;
-    return ExprRef<View>(static_pointer_cast<View>(this->getPtr()));
+    return static_pointer_cast<View>(this->getPtr());
 }
 
 template <typename ViewType>
@@ -47,7 +48,7 @@ typename std::enable_if<
     const ValRef<typename AssociatedValueType<ViewType>::type>>::type
 assumeAsValue(const ExprRef<ViewType>& viewPtr) {
     typedef typename AssociatedValueType<ViewType>::type ValueType;
-    return ValRef<ValueType>(static_pointer_cast<ValueType>(viewPtr.getPtr()));
+    return static_pointer_cast<ValueType>(viewPtr.getPtr());
 }
 
 template <typename ViewType>
@@ -56,11 +57,11 @@ typename std::enable_if<
     ValRef<typename AssociatedValueType<ViewType>::type>>::type
 assumeAsValue(ExprRef<ViewType>& viewPtr) {
     typedef typename AssociatedValueType<ViewType>::type ValueType;
-    return ValRef<ValueType>(static_pointer_cast<ValueType>(viewPtr.getPtr()));
+    return static_pointer_cast<ValueType>(viewPtr.getPtr());
 }
 
 #define exprInstantiators(name)                                   \
-    template struct ExprInterface<name##View>;                          \
+    template struct ExprInterface<name##View>;                    \
     template struct ExprRef<name##View>;                          \
     template struct ValRef<name##Value>;                          \
     template ValRef<name##Value> assumeAsValue<name##View>(       \

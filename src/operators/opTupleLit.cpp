0c87e80dc478cad40c655f10fcc8caac4a5a158c
saad_attieh <saad_attieh@me.com>
2018-04-29 15:01:18 +0100

    added op tuple lit

---
+++ src/operators/opTupleLit.cpp
@@ -0,0 +1,139 @@
+#include "operators/opTupleLit.h"
+#include <cassert>
+#include <cstdlib>
+#include "operators/simpleOperator.hpp"
+#include "types/allTypes.h"
+#include "utils/ignoreUnused.h"
+using namespace std;
+void OpTupleLit::evaluate() {
+    for (auto& member : members) {
+        mpark::visit([&](auto& member) { member->evaluate(); }, member);
+    }
+}
+template <typename TriggerType>
+struct ExprTrigger
+    : public OpTupleLit::ExprTriggerBase,
+      public ChangeTriggerAdapter<TriggerType, ExprTrigger<TriggerType>> {
+    using ExprTriggerBase::ExprTriggerBase;
+    void adapterPossibleValueChange() {
+        this->op->notifyPossibleMemberChange(this->index);
+    }
+    void adapterValueChanged() {
+        this->op->memberChangedAndNotify(this->index);
+    }
+
+    void reattachTrigger() final {
+        deleteTrigger(static_pointer_cast<ExprTrigger<TriggerType>>(
+            op->exprTriggers[index]));
+        auto trigger = make_shared<ExprTrigger<TriggerType>>(op, index);
+        mpark::get<ExprRef<typename AssociatedViewType<TriggerType>::type>>(
+            op->members[index])
+            ->addTrigger(trigger);
+        op->exprTriggers[index] = trigger;
+    }
+};
+
+OpTupleLit::OpTupleLit(OpTupleLit&& other)
+    : TupleView(std::move(other)), exprTriggers(std::move(other.exprTriggers)) {
+    setTriggerParent(this, exprTriggers);
+}
+
+void OpTupleLit::startTriggering() {
+    if (exprTriggers.empty()) {
+        for (size_t i = 0; i < members.size(); i++) {
+            mpark::visit(
+                [&](auto& member) {
+                    typedef
+                        typename AssociatedTriggerType<viewType(member)>::type
+                            TriggerType;
+
+                    auto trigger =
+                        make_shared<ExprTrigger<TriggerType>>(this, i);
+                    member->addTrigger(trigger);
+                    exprTriggers.emplace_back(move(trigger));
+                    member->startTriggering();
+                },
+                members[i]);
+        }
+    }
+}
+
+void OpTupleLit::stopTriggeringOnChildren() {
+    if (!exprTriggers.empty()) {
+        for (size_t i = 0; i < triggers.size(); i++) {
+            auto& trigger = exprTriggers[i];
+            mpark::visit(
+                [&](auto& member) {
+                    typedef
+                        typename AssociatedTriggerType<viewType(member)>::type
+                            TriggerType;
+                    deleteTrigger(
+                        static_pointer_cast<ExprTrigger<TriggerType>>(trigger));
+                },
+                members[i]);
+        }
+        exprTriggers.clear();
+    }
+}
+
+void OpTupleLit::stopTriggering() {
+    if (!exprTriggers.empty()) {
+        this->stopTriggeringOnChildren();
+        for (auto& member : members) {
+            mpark::visit([&](auto& member) { member->stopTriggering(); },
+                         member);
+        }
+    }
+}
+
+void OpTupleLit::updateViolationDescription(UInt, ViolationDescription&) {}
+
+ExprRef<TupleView> OpTupleLit::deepCopySelfForUnroll(
+    const ExprRef<TupleView>&, const AnyIterRef& iterator) const {
+    vector<AnyExprRef> newMembers;
+    for (auto& member : members) {
+        mpark::visit(
+            [&](auto& member) {
+                newMembers.emplace_back(
+                    member->deepCopySelfForUnroll(member, iterator));
+            },
+            member);
+    }
+    auto newOpTupleLit = make_shared<OpTupleLit>(move(newMembers));
+    return newOpTupleLit;
+}
+
+std::ostream& OpTupleLit::dumpState(std::ostream& os) const {
+    os << "OpTupleLit: (";
+    bool first = true;
+    for (const auto& member : members) {
+        if (first) {
+            first = false;
+        } else {
+            os << ",";
+        }
+        mpark::visit([&](auto& member) { member->dumpState(os); }, member);
+    }
+    os << ")";
+    return os;
+}
+
+void OpTupleLit::findAndReplaceSelf(const FindAndReplaceFunction& func) {
+    for (auto& member : members) {
+        mpark::visit(
+            [&](auto& member) { member = findAndReplace(member, func); },
+            member);
+    }
+}
+
+template <typename Op>
+struct OpMaker;
+
+template <>
+struct OpMaker<OpTupleLit> {
+    static ExprRef<TupleView> make(vector<AnyExprRef> members);
+};
+
+ExprRef<TupleView> OpMaker<OpTupleLit>::make(vector<AnyExprRef> o) {
+    return make_shared<OpTupleLit>(move(o));
+}
---
+++ src/operators/opTupleLit.h
@@ -0,0 +1,31 @@
+#ifndef SRC_OPERATORS_OPTUPLELIT_H_
+#define SRC_OPERATORS_OPTUPLELIT_H_
+#include "base/base.h"
+#include "types/tuple.h"
+
+struct OpTupleLit : public TupleView {
+    struct ExprTriggerBase {
+        OpTupleLit* op;
+        UInt index;
+        ExprTriggerBase(OpTupleLit* op, UInt index) : op(op), index(index) {}
+    };
+    std::vector<std::shared_ptr<ExprTriggerBase>> exprTriggers;
+
+    OpTupleLit(std::vector<AnyExprRef> members)
+        : TupleView(std::move(members)) {}
+    OpTupleLit(const OpTupleLit&) = delete;
+    OpTupleLit(OpTupleLit&& other);
+    ~OpTupleLit() { this->stopTriggeringOnChildren(); }
+    void evaluate() final;
+    void startTriggering() final;
+    void stopTriggering() final;
+    void stopTriggeringOnChildren();
+    void updateViolationDescription(UInt parentViolation,
+                                    ViolationDescription&) final;
+    ExprRef<TupleView> deepCopySelfForUnroll(
+        const ExprRef<TupleView>&, const AnyIterRef& iterator) const final;
+    std::ostream& dumpState(std::ostream& os) const final;
+    void findAndReplaceSelf(const FindAndReplaceFunction&) final;
+};
+
+#endif /* SRC_OPERATORS_OPTUPLELIT_H_ */

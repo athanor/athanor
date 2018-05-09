972066618beddc33db45e019519330798fe8db38
saad_attieh <saad_attieh@me.com>
2018-05-09 14:40:14 +0300

    this entire time a file has been untracked

---
+++ src/operators/simpleOperator.hpp
@@ -0,0 +1,130 @@
+
+#ifndef SRC_OPERATORS_SIMPLEOPERATOR_HPP_
+#define SRC_OPERATORS_SIMPLEOPERATOR_HPP_
+#include "base/base.h"
+#include "operators/simpleOperator.h"
+template <typename View, typename OperandView, typename Derived>
+void SimpleBinaryOperator<View, OperandView, Derived>::evaluate() {
+    left->evaluate();
+    right->evaluate();
+    bool defined = !(left->isUndefined() || right->isUndefined());
+    this->setDefined(defined);
+    if (defined) {
+        derived().reevaluate();
+    }
+}
+
+template <typename View, typename OperandView, typename Derived>
+void SimpleBinaryOperator<View, OperandView, Derived>::startTriggering() {
+    if (!leftTrigger) {
+        leftTrigger = std::make_shared<LeftTrigger>(&derived());
+        rightTrigger = std::make_shared<RightTrigger>(&derived());
+        left->addTrigger(leftTrigger);
+        right->addTrigger(rightTrigger);
+        left->startTriggering();
+        right->startTriggering();
+    }
+}
+
+template <typename View, typename OperandView, typename Derived>
+void SimpleBinaryOperator<View, OperandView,
+                          Derived>::stopTriggeringOnChildren() {
+    if (leftTrigger) {
+        deleteTrigger(leftTrigger);
+        leftTrigger = nullptr;
+        deleteTrigger(rightTrigger);
+        rightTrigger = nullptr;
+    }
+}
+
+template <typename View, typename OperandView, typename Derived>
+void SimpleBinaryOperator<View, OperandView, Derived>::stopTriggering() {
+    if (leftTrigger) {
+        stopTriggeringOnChildren();
+        left->stopTriggering();
+        right->stopTriggering();
+    }
+}
+
+template <typename View, typename OperandView, typename Derived>
+ExprRef<View>
+SimpleBinaryOperator<View, OperandView, Derived>::deepCopySelfForUnroll(
+    const ExprRef<View>&, const AnyIterRef& iterator) const {
+    auto newOp = std::make_shared<Derived>(
+        left->deepCopySelfForUnroll(left, iterator),
+        right->deepCopySelfForUnroll(right, iterator));
+    this->copyDefinedStatus(*newOp);
+    derived().copy(*newOp);
+    return newOp;
+}
+
+template <typename View, typename OperandView, typename Derived>
+void SimpleBinaryOperator<View, OperandView, Derived>::findAndReplaceSelf(
+    const FindAndReplaceFunction& func) {
+    this->left = findAndReplace(left, func);
+    this->right = findAndReplace(right, func);
+}
+
+template <typename View, typename OperandView, typename Derived>
+bool SimpleBinaryOperator<View, OperandView, Derived>::isUndefined() {
+    return !this->isDefined();
+}
+
+template <typename View, typename OperandView, typename Derived>
+void SimpleUnaryOperator<View, OperandView, Derived>::evaluate() {
+    operand->evaluate();
+    bool defined = !operand->isUndefined();
+    this->setDefined(defined);
+    if (defined) {
+        derived().reevaluate();
+    }
+}
+
+template <typename View, typename OperandView, typename Derived>
+void SimpleUnaryOperator<View, OperandView, Derived>::startTriggering() {
+    if (!operandTrigger) {
+        operandTrigger = std::make_shared<OperandTrigger>(&derived());
+        operand->addTrigger(operandTrigger);
+        operand->startTriggering();
+    }
+}
+
+template <typename View, typename OperandView, typename Derived>
+void SimpleUnaryOperator<View, OperandView,
+                         Derived>::stopTriggeringOnChildren() {
+    if (operandTrigger) {
+        deleteTrigger(operandTrigger);
+        operandTrigger = nullptr;
+    }
+}
+
+template <typename View, typename OperandView, typename Derived>
+void SimpleUnaryOperator<View, OperandView, Derived>::stopTriggering() {
+    if (operandTrigger) {
+        stopTriggeringOnChildren();
+        operand->stopTriggering();
+    }
+}
+
+template <typename View, typename OperandView, typename Derived>
+ExprRef<View>
+SimpleUnaryOperator<View, OperandView, Derived>::deepCopySelfForUnroll(
+    const ExprRef<View>&, const AnyIterRef& iterator) const {
+    auto newOp = std::make_shared<Derived>(
+        operand->deepCopySelfForUnroll(operand, iterator));
+    this->copyDefinedStatus(*newOp);
+    derived().copy(*newOp);
+    return newOp;
+}
+
+template <typename View, typename OperandView, typename Derived>
+void SimpleUnaryOperator<View, OperandView, Derived>::findAndReplaceSelf(
+    const FindAndReplaceFunction& func) {
+    this->operand = findAndReplace(operand, func);
+}
+
+template <typename View, typename OperandView, typename Derived>
+bool SimpleUnaryOperator<View, OperandView, Derived>::isUndefined() {
+    return !this->isDefined();
+}
+#endif /* SRC_OPERATORS_SIMPLEOPERATOR_HPP_ */

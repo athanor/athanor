c5b618f5dcee59caf784d3a82736ccc0c8c46502
saad_attieh <saad_attieh@me.com>
2018-05-09 19:39:03 +0300

    added missing file operator makers

---
+++ src/operators/operatorMakers.h
@@ -0,0 +1,85 @@
+
+#ifndef SRC_OPERATORS_OPERATORMAKERS_H_
+#define SRC_OPERATORS_OPERATORMAKERS_H_
+#include "base/base.h"
+
+template <typename Op>
+struct OpMaker;
+struct OpIntEq;
+template <>
+struct OpMaker<OpIntEq> {
+    static ExprRef<BoolView> make(ExprRef<IntView> l, ExprRef<IntView> r);
+};
+struct OpMod;
+template <>
+struct OpMaker<OpMod> {
+    static ExprRef<IntView> make(ExprRef<IntView> l, ExprRef<IntView> r);
+};
+struct OpAnd;
+template <>
+struct OpMaker<OpAnd> {
+    static ExprRef<BoolView> make(ExprRef<SequenceView>);
+};
+struct OpOr;
+template <>
+struct OpMaker<OpOr> {
+    static ExprRef<BoolView> make(ExprRef<SequenceView>);
+};
+struct OpProd;
+template <>
+struct OpMaker<OpProd> {
+    static ExprRef<IntView> make(ExprRef<SequenceView>);
+};
+struct OpSequenceLit;
+template <>
+struct OpMaker<OpSequenceLit> {
+    static ExprRef<SequenceView> make(AnyExprVec members);
+};
+struct OpSetNotEq;
+template <>
+struct OpMaker<OpSetNotEq> {
+    static ExprRef<BoolView> make(ExprRef<SetView> l, ExprRef<SetView> r);
+};
+struct OpSetSize;
+template <>
+struct OpMaker<OpSetSize> {
+    static ExprRef<IntView> make(ExprRef<SetView> o);
+};
+struct OpSubsetEq;
+template <>
+struct OpMaker<OpSubsetEq> {
+    static ExprRef<BoolView> make(ExprRef<SetView> l, ExprRef<SetView> r);
+};
+
+struct OpSum;
+template <>
+struct OpMaker<OpSum> {
+    static ExprRef<IntView> make(ExprRef<SequenceView>);
+};
+template <typename View>
+struct OpSequenceIndex;
+template <typename View>
+struct OpMaker<OpSequenceIndex<View>> {
+    static ExprRef<View> make(ExprRef<SequenceView> sequence,
+                              ExprRef<IntView> index);
+};
+template <typename View>
+struct OpTupleIndex;
+
+template <typename View>
+struct OpMaker<OpTupleIndex<View>> {
+    static ExprRef<View> make(ExprRef<TupleView> tuple, UInt index);
+};
+
+struct OpTupleLit;
+template <>
+struct OpMaker<OpTupleLit> {
+    static ExprRef<TupleView> make(std::vector<AnyExprRef> members);
+};
+
+struct OpIsDefined;
+template <>
+struct OpMaker<OpIsDefined> {
+    static ExprRef<BoolView> make(ExprRef<IntView> o);
+};
+#endif /* SRC_OPERATORS_OPERATORMAKERS_H_ */

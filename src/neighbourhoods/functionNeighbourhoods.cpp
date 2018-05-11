2315b04a2f23c4c85a2134450258772280c47f88
saad_attieh <saad_attieh@me.com>
2018-05-11 21:15:06 +0300

    added required function stub assignRandomValueInDomain<FunctionValue>

---
+++ src/neighbourhoods/functionNeighbourhoods.cpp
@@ -0,0 +1,16 @@
+#include <cmath>
+#include <random>
+#include "neighbourhoods/neighbourhoods.h"
+#include "types/function.h"
+#include "utils/random.h"
+
+using namespace std;
+static ViolationDescription emptyViolations;
+
+template <>
+void assignRandomValueInDomain<FunctionDomain>(const FunctionDomain&,
+                                               FunctionValue&) {
+    todoImpl();
+}
+const NeighbourhoodVec<FunctionDomain>
+    NeighbourhoodGenList<FunctionDomain>::value = {};

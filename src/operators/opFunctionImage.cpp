fd4c8f372665760aa83f7f41ad36e4d6a2d95012
saad_attieh <saad_attieh@me.com>
2018-05-14 13:08:48 +0300

    added op function image

---
+++ src/operators/opFunctionImage.cpp
@@ -0,0 +1,322 @@
+#include "operators/opFunctionImage.h"
+#include <iostream>
+#include <memory>
+#include "types/allTypes.h"
+#include "utils/ignoreUnused.h"
+using namespace std;
+#define invoke(expr, func) \
+    mpark::visit([&](auto& expr) { return expr->func; }, expr)
+
+#define invoke_r(expr, func, ret) \
+    mpark::visit([&](auto& expr) -> ret { return expr->func; }, expr)
+
+template <typename FunctionMemberViewType>
+void OpFunctionImage<FunctionMemberViewType>::addTrigger(
+    const shared_ptr<FunctionMemberTriggerType>& trigger) {
+    triggers.emplace_back(getTriggerBase(trigger));
+    if (locallyDefined) {
+        getMember()->addTrigger(trigger);
+    }
+}
+
+template <typename FunctionMemberViewType>
+ExprRef<FunctionMemberViewType>&
+OpFunctionImage<FunctionMemberViewType>::getMember() {
+    debug_code(assert(locallyDefined));
+    debug_code(assert(!invoke(preImageOperand, isUndefined()) &&
+                      cachedIndex >= 0 && !functionOperand->isUndefined() &&
+                      cachedIndex < (Int)functionOperand->view().rangeSize()));
+    auto& member =
+        functionOperand->view().getRange<FunctionMemberViewType>()[cachedIndex];
+    debug_code(assert(!member->isUndefined()));
+    return member;
+}
+
+template <typename FunctionMemberViewType>
+const ExprRef<FunctionMemberViewType>&
+OpFunctionImage<FunctionMemberViewType>::getMember() const {
+    debug_code(assert(locallyDefined));
+    debug_code(assert(!invoke(preImageOperand, isUndefined()) &&
+                      cachedIndex >= 0 && !functionOperand->isUndefined() &&
+                      cachedIndex < (Int)functionOperand->view().rangeSize()));
+
+    const auto& member =
+        functionOperand->view().getRange<FunctionMemberViewType>()[cachedIndex];
+    debug_code(assert(!member->isUndefined()));
+    return member;
+}
+
+template <typename FunctionMemberViewType>
+FunctionMemberViewType& OpFunctionImage<FunctionMemberViewType>::view() {
+    return getMember()->view();
+}
+template <typename FunctionMemberViewType>
+const FunctionMemberViewType& OpFunctionImage<FunctionMemberViewType>::view()
+    const {
+    return getMember()->view();
+}
+
+template <typename FunctionMemberViewType>
+void OpFunctionImage<FunctionMemberViewType>::reevaluate() {
+    if (invoke(preImageOperand, isUndefined())) {
+        locallyDefined = false;
+        return;
+    }
+    auto boolIndexPair = mpark::visit(
+        [&](auto& preImageOperand) {
+            return functionOperand->view().domainToIndex(
+                preImageOperand->view());
+        },
+        preImageOperand);
+    if (!boolIndexPair.first) {
+        locallyDefined = false;
+    } else {
+        locallyDefined = true;
+        cachedIndex = boolIndexPair.second;
+    }
+}
+
+template <typename FunctionMemberViewType>
+void OpFunctionImage<FunctionMemberViewType>::evaluate() {
+    functionOperand->evaluate();
+    invoke(preImageOperand, evaluate());
+    reevaluate();
+}
+
+template <typename FunctionMemberViewType>
+OpFunctionImage<FunctionMemberViewType>::OpFunctionImage(
+    OpFunctionImage<FunctionMemberViewType>&& other)
+    : ExprInterface<FunctionMemberViewType>(move(other)),
+      triggers(move(other.triggers)),
+      functionOperand(move(other.functionOperand)),
+      preImageOperand(move(other.preImageOperand)),
+      cachedIndex(move(other.cachedIndex)),
+      locallyDefined(move(other.locallyDefined)),
+      preImageTrigger(move(other.preImageTrigger)) {
+    setTriggerParent(this, preImageTrigger);
+}
+
+template <typename FunctionMemberViewType, typename PreImageTriggerType,
+          typename Derived>
+struct PreImageTriggerHelper
+    : public OpFunctionImage<FunctionMemberViewType>::PreImageTriggerBase,
+      public ChangeTriggerAdapter<PreImageTriggerType, Derived> {
+    using OpFunctionImage<
+        FunctionMemberViewType>::PreImageTriggerBase::PreImageTriggerBase;
+};
+
+template <typename FunctionMemberViewType, typename Derived>
+struct PreImageTriggerHelper<FunctionMemberViewType, TupleTrigger, Derived>
+    : public OpFunctionImage<FunctionMemberViewType>::PreImageTriggerBase,
+      public ChangeTriggerAdapter<TupleTrigger, Derived> {
+    using OpFunctionImage<
+        FunctionMemberViewType>::PreImageTriggerBase::PreImageTriggerBase;
+    void memberHasBecomeDefined(UInt) final {
+        if (mpark::get<ExprRef<TupleView>>(this->op->preImageOperand)
+                ->view()
+                .numberUndefined == 0) {
+            static_cast<Derived&>(*this).adapterHasBecomeDefined();
+        }
+    }
+    void memberHasBecomeUndefined(UInt) final {
+        if (mpark::get<ExprRef<TupleView>>(this->op->preImageOperand)
+                ->view()
+                .numberUndefined == 1) {
+            static_cast<Derived&>(*this).adapterHasBecomeUndefined();
+        }
+    }
+};
+
+template <typename FunctionMemberViewType, typename TriggerType>
+struct PreImageTrigger
+    : public PreImageTriggerHelper<
+          FunctionMemberViewType, TriggerType,
+          PreImageTrigger<FunctionMemberViewType, TriggerType>> {
+    typedef typename AssociatedViewType<TriggerType>::type PreImageType;
+    using PreImageTriggerHelper<
+        FunctionMemberViewType, TriggerType,
+        PreImageTrigger<FunctionMemberViewType,
+                        TriggerType>>::PreImageTriggerHelper;
+
+    void adapterPossibleValueChange() {
+        if (!this->op->locallyDefined) {
+            return;
+        }
+
+        visitTriggers([&](auto& t) { t->possibleValueChange(); },
+                      this->op->triggers);
+    }
+
+    void adapterValueChanged() {
+        bool wasDefined = this->op->locallyDefined;
+        this->op->reevaluate();
+        if (wasDefined && !this->op->locallyDefined) {
+            visitTriggers([&](auto& t) { t->hasBecomeUndefined(); },
+                          this->op->triggers);
+        } else if (!wasDefined && this->op->locallyDefined) {
+            visitTriggers(
+                [&](auto& t) {
+                    t->hasBecomeDefined();
+                    t->reattachTrigger();
+                },
+                this->op->triggers);
+        } else if (this->op->locallyDefined) {
+            visitTriggers(
+                [&](auto& t) {
+                    t->valueChanged();
+                    t->reattachTrigger();
+                },
+                this->op->triggers);
+        }
+    }
+
+    void reattachTrigger() final {
+        deleteTrigger(static_pointer_cast<
+                      PreImageTrigger<FunctionMemberViewType, TriggerType>>(
+            this->op->preImageTrigger));
+        auto trigger =
+            make_shared<PreImageTrigger<FunctionMemberViewType, TriggerType>>(
+                this->op);
+        mpark::get<ExprRef<PreImageType>>(this->op->preImageOperand)
+            ->addTrigger(trigger);
+        this->op->preImageTrigger = trigger;
+    }
+    void adapterHasBecomeDefined() {
+        this->op->reevaluate();
+        if (!this->op->locallyDefined) {
+            return;
+        }
+        visitTriggers(
+            [&](auto& t) {
+                t->hasBecomeDefined();
+                t->reattachTrigger();
+            },
+            this->op->triggers);
+    }
+
+    void adapterHasBecomeUndefined() {
+        if (!this->op->locallyDefined) {
+            return;
+        }
+        this->op->locallyDefined = false;
+        visitTriggers([&](auto& t) { t->hasBecomeUndefined(); },
+                      this->op->triggers);
+    }
+};
+
+template <typename FunctionMemberViewType>
+void OpFunctionImage<FunctionMemberViewType>::startTriggering() {
+    if (!preImageTrigger) {
+        mpark::visit(
+            [&](auto& preImageOperand) {
+                typedef typename AssociatedTriggerType<viewType(
+                    preImageOperand)>::type TriggerType;
+                auto trigger = make_shared<
+                    PreImageTrigger<FunctionMemberViewType, TriggerType>>(this);
+                preImageTrigger = trigger;
+                preImageOperand->addTrigger(trigger);
+                preImageOperand->startTriggering();
+            },
+            preImageOperand);
+    }
+}
+
+template <typename FunctionMemberViewType>
+void OpFunctionImage<FunctionMemberViewType>::stopTriggeringOnChildren() {
+    if (preImageTrigger) {
+        mpark::visit(
+            [&](auto& preImageOperand) {
+                typedef typename AssociatedTriggerType<viewType(
+                    preImageOperand)>::type PreImageTriggerType;
+                deleteTrigger(
+                    static_pointer_cast<PreImageTrigger<FunctionMemberViewType,
+                                                        PreImageTriggerType>>(
+                        preImageTrigger));
+            },
+            preImageOperand);
+        preImageTrigger = nullptr;
+    }
+}
+
+template <typename FunctionMemberViewType>
+void OpFunctionImage<FunctionMemberViewType>::stopTriggering() {
+    if (preImageTrigger) {
+        stopTriggeringOnChildren();
+        invoke(preImageOperand, stopTriggering());
+        ;
+    }
+}
+
+template <typename FunctionMemberViewType>
+void OpFunctionImage<FunctionMemberViewType>::updateViolationDescription(
+    UInt parentViolation, ViolationDescription& vioDesc) {
+    invoke(preImageOperand,
+           updateViolationDescription(parentViolation, vioDesc));
+    if (locallyDefined) {
+        getMember()->updateViolationDescription(parentViolation, vioDesc);
+    } else {
+        functionOperand->updateViolationDescription(parentViolation, vioDesc);
+    }
+}
+
+template <typename FunctionMemberViewType>
+ExprRef<FunctionMemberViewType>
+OpFunctionImage<FunctionMemberViewType>::deepCopySelfForUnroll(
+    const ExprRef<FunctionMemberViewType>&, const AnyIterRef& iterator) const {
+    auto newOpFunctionImage =
+        make_shared<OpFunctionImage<FunctionMemberViewType>>(
+            functionOperand->deepCopySelfForUnroll(functionOperand, iterator),
+            invoke_r(preImageOperand,
+                     deepCopySelfForUnroll(preImageOperand, iterator),
+                     AnyExprRef));
+    newOpFunctionImage->cachedIndex = cachedIndex;
+    newOpFunctionImage->locallyDefined = locallyDefined;
+    return newOpFunctionImage;
+}
+
+template <typename FunctionMemberViewType>
+std::ostream& OpFunctionImage<FunctionMemberViewType>::dumpState(
+    std::ostream& os) const {
+    os << "opFunctionImage(function=";
+    functionOperand->dumpState(os) << ",\n";
+    os << "preImage=";
+    invoke_r(preImageOperand, dumpState(os), ostream&) << ")";
+    return os;
+}
+
+template <typename FunctionMemberViewType>
+void OpFunctionImage<FunctionMemberViewType>::findAndReplaceSelf(
+    const FindAndReplaceFunction& func) {
+    this->functionOperand = findAndReplace(functionOperand, func);
+    this->preImageOperand = mpark::visit(
+        [&](auto& preImageOperand) -> AnyExprRef {
+            return findAndReplace(preImageOperand, func);
+        },
+        preImageOperand);
+}
+
+template <typename FunctionMemberViewType>
+bool OpFunctionImage<FunctionMemberViewType>::isUndefined() {
+    return !locallyDefined;
+}
+template <typename Op>
+struct OpMaker;
+
+template <typename View>
+struct OpMaker<OpFunctionImage<View>> {
+    static ExprRef<View> make(ExprRef<FunctionView> function,
+                              AnyExprRef preImage);
+};
+
+template <typename View>
+ExprRef<View> OpMaker<OpFunctionImage<View>>::make(
+    ExprRef<FunctionView> function, AnyExprRef preImage) {
+    return make_shared<OpFunctionImage<View>>(move(function), move(preImage));
+}
+
+#define opFunctionImageInstantiators(name)       \
+    template struct OpFunctionImage<name##View>; \
+    template struct OpMaker<OpFunctionImage<name##View>>;
+
+buildForAllTypes(opFunctionImageInstantiators, );
+#undef opFunctionImageInstantiators
---
+++ src/operators/opFunctionImage.h
@@ -0,0 +1,55 @@
+
+#ifndef SRC_OPERATORS_OPFUNCTIONIMAGE_H_
+#define SRC_OPERATORS_OPFUNCTIONIMAGE_H_
+
+#include "base/base.h"
+#include "types/function.h"
+#include "types/int.h"
+template <typename FunctionMemberViewType>
+struct OpFunctionImage : public ExprInterface<FunctionMemberViewType> {
+    struct PreImageTriggerBase {
+        OpFunctionImage<FunctionMemberViewType>* op;
+        PreImageTriggerBase(OpFunctionImage<FunctionMemberViewType>* op)
+            : op(op) {}
+        virtual ~PreImageTriggerBase() {}
+    };
+
+    typedef typename AssociatedTriggerType<FunctionMemberViewType>::type
+        FunctionMemberTriggerType;
+    std::vector<std::shared_ptr<TriggerBase>> triggers;
+    ExprRef<FunctionView> functionOperand;
+    AnyExprRef preImageOperand;
+    Int cachedIndex;
+    bool locallyDefined = false;
+    std::shared_ptr<PreImageTriggerBase> preImageTrigger;
+    OpFunctionImage(ExprRef<FunctionView> functionOperand,
+                    AnyExprRef preImageOperand)
+        : functionOperand(std::move(functionOperand)),
+          preImageOperand(std::move(preImageOperand)) {}
+    OpFunctionImage(const OpFunctionImage<FunctionMemberViewType>&) = delete;
+    OpFunctionImage(OpFunctionImage<FunctionMemberViewType>&& other);
+    ~OpFunctionImage() { this->stopTriggeringOnChildren(); }
+    void addTrigger(
+        const std::shared_ptr<FunctionMemberTriggerType>& trigger) final;
+    FunctionMemberViewType& view() final;
+    const FunctionMemberViewType& view() const final;
+
+    void evaluate() final;
+    void startTriggering() final;
+    void stopTriggering() final;
+    void stopTriggeringOnChildren();
+
+    void updateViolationDescription(UInt parentViolation,
+                                    ViolationDescription&) final;
+    ExprRef<FunctionMemberViewType> deepCopySelfForUnroll(
+        const ExprRef<FunctionMemberViewType>& self,
+        const AnyIterRef& iterator) const final;
+    std::ostream& dumpState(std::ostream& os) const final;
+    void findAndReplaceSelf(const FindAndReplaceFunction&) final;
+    bool isUndefined();
+    ExprRef<FunctionMemberViewType>& getMember();
+    const ExprRef<FunctionMemberViewType>& getMember() const;
+    void reevaluate();
+};
+
+#endif /* SRC_OPERATORS_OPFUNCTIONIMAGE_H_ */

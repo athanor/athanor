0c59de7bbfa9b6662d78dee0760cdf520fe377bd
saad_attieh <saad_attieh@me.com>
2018-05-19 18:07:08 +0300

    added op to int

---
+++ src/operators/opToInt.cpp
@@ -0,0 +1,33 @@
+#include "operators/opToInt.h"
+#include <iostream>
+#include <memory>
+#include "operators/simpleOperator.hpp"
+#include "types/int.h"
+
+using namespace std;
+
+void OpToInt::reevaluate() { value = (operand->view().violation == 0) ? 1 : 0; }
+void OpToInt::updateViolationDescription(UInt parentViolation,
+                                         ViolationDescription& vioDesc) {
+    operand->updateViolationDescription(parentViolation, vioDesc);
+}
+
+void OpToInt::copy(OpToInt& newOp) const { newOp.value = value; }
+
+std::ostream& OpToInt::dumpState(std::ostream& os) const {
+    os << "OpToInt: value=" << value << "\noperand: ";
+    operand->dumpState(os);
+    return os;
+}
+
+template <typename Op>
+struct OpMaker;
+
+template <>
+struct OpMaker<OpToInt> {
+    static ExprRef<IntView> make(ExprRef<BoolView> o);
+};
+
+ExprRef<IntView> OpMaker<OpToInt>::make(ExprRef<BoolView> o) {
+    return make_shared<OpToInt>(move(o));
+}
---
+++ src/operators/opToInt.h
@@ -0,0 +1,22 @@
+
+#ifndef SRC_OPERATORS_OPTOINT_H_
+#define SRC_OPERATORS_OPTOINT_H_
+#include "operators/simpleOperator.h"
+#include "operators/simpleTrigger.h"
+#include "types/bool.h"
+#include "types/int.h"
+struct OpToInt;
+template <>
+struct OperatorTrates<OpToInt> {
+    typedef SimpleUnaryTrigger<OpToInt, BoolTrigger> OperandTrigger;
+};
+struct OpToInt : public SimpleUnaryOperator<IntView, BoolView, OpToInt> {
+    using SimpleUnaryOperator<IntView, BoolView, OpToInt>::SimpleUnaryOperator;
+
+    void reevaluate();
+    void updateViolationDescription(UInt parentViolation,
+                                    ViolationDescription& vioDesc) final;
+    void copy(OpToInt& newOp) const;
+    std::ostream& dumpState(std::ostream& os) const final;
+};
+#endif /* SRC_OPERATORS_OPTOINT_H_ */

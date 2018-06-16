0c26afa3e6db1c947cd02c3e71392ccc80b04e2f
saad_attieh <saad_attieh@me.com>
2018-06-16 21:39:46 +0200

    updated sequence

--- src/neighbourhoods/sequenceNeighbourhoods.cpp
+++ src/neighbourhoods/sequenceNeighbourhoods.cpp
@@ -43,18 +43,19 @@ void assignRandomValueInDomain<SequenceDomain>(const SequenceDomain& domain,
 template <typename InnerDomainPtrType>
 void sequenceLiftSingleGenImpl(const SequenceDomain& domain,
                                const InnerDomainPtrType& innerDomainPtr,
+                               int numberValsRequired,
                                std::vector<Neighbourhood>& neighbourhoods) {
     std::vector<Neighbourhood> innerDomainNeighbourhoods;
-    generateNeighbourhoods(domain.inner, innerDomainNeighbourhoods);
+    generateNeighbourhoods(1, domain.inner, innerDomainNeighbourhoods);
     UInt innerDomainSize = getDomainSize(domain.inner);
     typedef typename AssociatedValueType<
         typename InnerDomainPtrType::element_type>::type InnerValueType;
     for (auto& innerNh : innerDomainNeighbourhoods) {
         neighbourhoods.emplace_back(
-            "sequenceLiftSingle_" + innerNh.name,
+            "sequenceLiftSingle_" + innerNh.name, numberValsRequired,
             [innerNhApply{std::move(innerNh.apply)}, innerDomainSize, &domain,
              &innerDomainPtr](NeighbourhoodParams& params) {
-                auto& val = *mpark::get<ValRef<SequenceValue>>(params.primary);
+                auto& val = *(params.getVals<SequenceValue>().front());
                 if (val.numberElements() == 0) {
                     ++params.stats.minorNodeCount;
                     return;
@@ -69,29 +70,34 @@ void sequenceLiftSingleGenImpl(const SequenceDomain& domain,
                         : globalRandom<UInt>(0, val.numberElements() - 1);
                 val.notifyPossibleSubsequenceChange<InnerValueType>(
                     indexToChange, indexToChange + 1);
-                ParentCheckCallBack parentCheck =
-                    [&](const AnyValRef& newValue) {
-                        HashType newHash = getValueHash(newValue);
-                        return !val.memberHashes.count(newHash) &&
-                               val.trySubsequenceChange<InnerValueType>(
-                                   indexToChange, indexToChange + 1, [&]() {
-                                       return params.parentCheck(
-                                           params.primary);
-                                   });
-                    };
+                ParentCheckCallBack parentCheck = [&](const AnyValVec&
+                                                          newValue) {
+                    HashType newHash = getValueHash(
+                        mpark::get<ValRefVec<InnerValueType>>(newValue)
+                            .front());
+                    return !val.memberHashes.count(newHash) &&
+                           val.trySubsequenceChange<InnerValueType>(
+                               indexToChange, indexToChange + 1, [&]() {
+                                   return params.parentCheck(params.vals);
+                               });
+                };
                 bool requiresRevert = false;
-                AnyValRef changingMember =
-                    val.member<InnerValueType>(indexToChange);
                 AcceptanceCallBack changeAccepted = [&]() {
                     requiresRevert = !params.changeAccepted();
                     if (requiresRevert) {
-                        val.notifyPossibleSubsequenceChange<InnerValueType>(
-                            indexToChange, indexToChange + 1);
+                        val.notifyPossibleSubsequenceChange<
+                            InnerValueType>(indexToChange, indexToChange + 1);
                     }
                     return !requiresRevert;
                 };
+                AnyValVec changingMembers;
+                auto& changingMembersImpl =
+                    changingMembers.emplace<ValRefVec<InnerValueType>>();
+                changingMembersImpl.emplace_back(
+                    val.member<InnerValueType>(indexToChange));
+
                 NeighbourhoodParams innerNhParams(
-                    changeAccepted, parentCheck, 1, changingMember,
+                    changeAccepted, parentCheck, 1, changingMembers,
                     params.stats, vioDescAtThisLevel);
                 innerNhApply(innerNhParams);
                 if (requiresRevert) {
@@ -103,11 +109,12 @@ void sequenceLiftSingleGenImpl(const SequenceDomain& domain,
     }
 }
 
-void sequenceLiftSingleGen(const SequenceDomain& domain,
+void sequenceLiftSingleGen(const SequenceDomain& domain, int numberValsRequired,
                            std::vector<Neighbourhood>& neighbourhoods) {
     mpark::visit(
         [&](const auto& innerDomainPtr) {
-            sequenceLiftSingleGenImpl(domain, innerDomainPtr, neighbourhoods);
+            sequenceLiftSingleGenImpl(domain, innerDomainPtr,
+                                      numberValsRequired, neighbourhoods);
         },
         domain.inner);
 }
@@ -115,13 +122,15 @@ void sequenceLiftSingleGen(const SequenceDomain& domain,
 template <typename InnerDomainPtrType>
 void sequenceAddGenImpl(const SequenceDomain& domain,
                         InnerDomainPtrType& innerDomainPtr,
+                        int numberValsRequired,
                         std::vector<Neighbourhood>& neighbourhoods) {
     typedef typename AssociatedValueType<
         typename InnerDomainPtrType::element_type>::type InnerValueType;
 
     neighbourhoods.emplace_back(
-        "sequenceAdd", [&domain, &innerDomainPtr](NeighbourhoodParams& params) {
-            auto& val = *mpark::get<ValRef<SequenceValue>>(params.primary);
+        "sequenceAdd", numberValsRequired,
+        [&domain, &innerDomainPtr](NeighbourhoodParams& params) {
+            auto& val = *(params.getVals<SequenceValue>().front());
             if (val.numberElements() == domain.sizeAttr.maxSize) {
                 ++params.stats.minorNodeCount;
                 return;
@@ -136,9 +145,9 @@ void sequenceAddGenImpl(const SequenceDomain& domain,
                                           params.stats);
                 size_t indexOfNewMember =
                     globalRandom<UInt>(0, val.numberElements());
-                success = val.tryAddMember(indexOfNewMember, newMember, [&]() {
-                    return params.parentCheck(params.primary);
-                });
+                success = val.tryAddMember(
+                    indexOfNewMember, newMember,
+                    [&]() { return params.parentCheck(params.vals); });
             } while (!success && ++numberTries < tryLimit);
             if (!success) {
                 debug_neighbourhood_action(
@@ -148,33 +157,35 @@ void sequenceAddGenImpl(const SequenceDomain& domain,
             debug_neighbourhood_action("Added value: " << newMember);
             if (!params.changeAccepted()) {
                 debug_neighbourhood_action("Change rejected");
-                val.tryRemoveMember<InnerValueType>(val.numberElements() - 1,
-                                                    []() { return true; });
+                val.tryRemoveMember<InnerValueType>(
+                    val.numberElements() - 1, []() { return true; });
             }
         });
 }
 
-void sequenceAddGen(const SequenceDomain& domain,
+void sequenceAddGen(const SequenceDomain& domain, int numberValsRequired,
                     std::vector<Neighbourhood>& neighbourhoods) {
     if (domain.sizeAttr.sizeType == SizeAttr::SizeAttrType::EXACT_SIZE) {
         return;
     }
     mpark::visit(
         [&](const auto& innerDomainPtr) {
-            sequenceAddGenImpl(domain, innerDomainPtr, neighbourhoods);
+            sequenceAddGenImpl(domain, innerDomainPtr, numberValsRequired,
+                               neighbourhoods);
         },
         domain.inner);
 }
 
 template <typename InnerDomainPtrType>
 void sequenceRemoveGenImpl(const SequenceDomain& domain, InnerDomainPtrType&,
+                           int numberValsRequired,
                            std::vector<Neighbourhood>& neighbourhoods) {
     typedef typename AssociatedValueType<
         typename InnerDomainPtrType::element_type>::type InnerValueType;
     neighbourhoods.emplace_back(
-        "sequenceRemove", [&](NeighbourhoodParams& params) {
+        "sequenceRemove", numberValsRequired, [&](NeighbourhoodParams& params) {
             ++params.stats.minorNodeCount;
-            auto& val = *mpark::get<ValRef<SequenceValue>>(params.primary);
+            auto& val = *(params.getVals<SequenceValue>().front());
             if (val.numberElements() == domain.sizeAttr.minSize) {
                 ++params.stats.minorNodeCount;
                 return;
@@ -189,9 +200,9 @@ void sequenceRemoveGenImpl(const SequenceDomain& domain, InnerDomainPtrType&,
                 indexToRemove =
                     globalRandom<size_t>(0, val.numberElements() - 1);
                 std::pair<bool, ValRef<InnerValueType>> removeStatus =
-                    val.tryRemoveMember<InnerValueType>(indexToRemove, [&]() {
-                        return params.parentCheck(params.primary);
-                    });
+                    val.tryRemoveMember<InnerValueType>(
+                        indexToRemove,
+                        [&]() { return params.parentCheck(params.vals); });
                 success = removeStatus.first;
                 if (success) {
                     removedMember = std::move(removeStatus.second);
@@ -205,20 +216,22 @@ void sequenceRemoveGenImpl(const SequenceDomain& domain, InnerDomainPtrType&,
             }
             if (!params.changeAccepted()) {
                 debug_neighbourhood_action("Change rejected");
-                val.tryAddMember(indexToRemove, std::move(removedMember),
-                                 []() { return true; });
+                val.tryAddMember(indexToRemove,
+                                          std::move(removedMember),
+                                          []() { return true; });
             }
         });
 }
 
-void sequenceRemoveGen(const SequenceDomain& domain,
+void sequenceRemoveGen(const SequenceDomain& domain, int numberValsRequired,
                        std::vector<Neighbourhood>& neighbourhoods) {
     if (domain.sizeAttr.sizeType == SizeAttr::SizeAttrType::EXACT_SIZE) {
         return;
     }
     mpark::visit(
         [&](const auto& innerDomainPtr) {
-            sequenceRemoveGenImpl(domain, innerDomainPtr, neighbourhoods);
+            sequenceRemoveGenImpl(domain, innerDomainPtr, numberValsRequired,
+                                  neighbourhoods);
         },
         domain.inner);
 }
@@ -226,14 +239,15 @@ void sequenceRemoveGen(const SequenceDomain& domain,
 template <typename InnerDomainPtrType>
 void sequencePositionsSwapGenImpl(const SequenceDomain& domain,
                                   InnerDomainPtrType& innerDomainPtr,
+                                  int numberValsRequired,
                                   std::vector<Neighbourhood>& neighbourhoods) {
     typedef typename AssociatedValueType<
         typename InnerDomainPtrType::element_type>::type InnerValueType;
 
     neighbourhoods.emplace_back(
-        "sequencePositionsSwap",
+        "sequencePositionsSwap", numberValsRequired,
         [&domain, &innerDomainPtr](NeighbourhoodParams& params) {
-            auto& val = *mpark::get<ValRef<SequenceValue>>(params.primary);
+            auto& val = *(params.getVals<SequenceValue>().front());
             if (val.numberElements() < 2) {
                 ++params.stats.minorNodeCount;
                 return;
@@ -250,7 +264,7 @@ void sequencePositionsSwapGenImpl(const SequenceDomain& domain,
                     globalRandom<UInt>(index1 + 1, val.numberElements() - 1);
                 success = val.trySwapPositions<InnerValueType>(
                     index1, index2,
-                    [&]() { return params.parentCheck(params.primary); });
+                    [&]() { return params.parentCheck(params.vals); });
             } while (!success && ++numberTries < tryLimit);
             if (!success) {
                 debug_neighbourhood_action(
@@ -262,27 +276,30 @@ void sequencePositionsSwapGenImpl(const SequenceDomain& domain,
                 "positions swapped: " << index1 << " and " << index2);
             if (!params.changeAccepted()) {
                 debug_neighbourhood_action("Change rejected");
-                val.trySwapPositions<InnerValueType>(index1, index2,
-                                                     []() { return true; });
+                val.trySwapPositions<InnerValueType>(
+                    index1, index2, []() { return true; });
             }
         });
 }
 
 void sequencePositionsSwapGen(const SequenceDomain& domain,
+                              int numberValsRequired,
                               std::vector<Neighbourhood>& neighbourhoods) {
     mpark::visit(
         [&](const auto& innerDomainPtr) {
             sequencePositionsSwapGenImpl(domain, innerDomainPtr,
-                                         neighbourhoods);
+                                         numberValsRequired, neighbourhoods);
         },
         domain.inner);
 }
 
 void sequenceAssignRandomGen(const SequenceDomain& domain,
+                             int numberValsRequired,
                              std::vector<Neighbourhood>& neighbourhoods) {
     neighbourhoods.emplace_back(
-        "sequenceAssignRandom", [&domain](NeighbourhoodParams& params) {
-            auto& val = *mpark::get<ValRef<SequenceValue>>(params.primary);
+        "sequenceAssignRandom", numberValsRequired,
+        [&domain](NeighbourhoodParams& params) {
+            auto& val = *(params.getVals<SequenceValue>().front());
             int numberTries = 0;
             const int tryLimit = params.parentCheckTryLimit;
             debug_neighbourhood_action(
@@ -295,7 +312,7 @@ void sequenceAssignRandomGen(const SequenceDomain& domain,
             do {
                 assignRandomValueInDomain(domain, *newValue, params.stats);
                 success = val.tryAssignNewValue(*newValue, [&]() {
-                    return params.parentCheck(params.primary);
+                    return params.parentCheck(params.vals);
                 });
                 if (success) {
                     debug_neighbourhood_action("New value is " << asView(val));
@@ -315,5 +332,7 @@ void sequenceAssignRandomGen(const SequenceDomain& domain,
 
 const NeighbourhoodVec<SequenceDomain>
     NeighbourhoodGenList<SequenceDomain>::value = {
-        sequenceLiftSingleGen, sequenceAddGen, sequenceRemoveGen,
-        sequencePositionsSwapGen};
+        {1, sequenceLiftSingleGen},
+        {1, sequenceAddGen},
+        {1, sequenceRemoveGen},
+        {1, sequencePositionsSwapGen}};

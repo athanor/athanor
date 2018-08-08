adc0efaaa84d46140d61dfc50b1584ef11c51ab7
saad_attieh <saad_attieh@me.com>
2018-08-08 19:03:35 +0100

    added op catch undef impl untested and stil to add parsing

---
+++ src/operators/opCatchUndef.cpp
@@ -0,0 +1,233 @@
+#include "operators/opCatchUndef.h"
+#include <iostream>
+#include <memory>
+#include "types/allTypes.h"
+#include "utils/ignoreUnused.h"
+using namespace std;
+
+template <typename ExprViewType>
+void OpCatchUndef<ExprViewType>::addTriggerImpl(
+    const shared_ptr<ExprTriggerType>& trigger, bool includeMembers,
+    Int memberIndex) {
+    triggers.emplace_back(getTriggerBase(trigger));
+    if (exprDefined) {
+        getMember()->addTrigger(trigger, includeMembers, memberIndex);
+    }
+}
+
+template <typename ExprViewType>
+ExprRef<ExprViewType>& OpCatchUndef<ExprViewType>::getMember() {
+    return (exprDefined) ? expr : replacement;
+}
+
+template <typename ExprViewType>
+const ExprRef<ExprViewType>& OpCatchUndef<ExprViewType>::getMember() const {
+    return (exprDefined) ? expr : replacement;
+}
+
+template <typename ExprViewType>
+ExprViewType& OpCatchUndef<ExprViewType>::view() {
+    return getMember()->view();
+}
+template <typename ExprViewType>
+const ExprViewType& OpCatchUndef<ExprViewType>::view() const {
+    return getMember()->view();
+}
+
+template <typename ExprViewType>
+void OpCatchUndef<ExprViewType>::reevaluate() {
+    exprDefined = !expr->isUndefined();
+}
+
+template <typename ExprViewType>
+void OpCatchUndef<ExprViewType>::evaluateImpl() {
+    replacement->evaluate();
+    ;
+    expr->evaluate();
+    reevaluate();
+}
+
+template <typename ExprViewType>
+OpCatchUndef<ExprViewType>::OpCatchUndef(OpCatchUndef<ExprViewType>&& other)
+    : ExprInterface<ExprViewType>(move(other)),
+      triggers(move(other.triggers)),
+      expr(move(other.expr)),
+      replacement(move(other.replacement)),
+      exprDefined(move(other.exprDefined)),
+      exprTrigger(move(other.exprTrigger)) {
+    setTriggerParent(this, exprTrigger);
+}
+namespace {
+template <typename ExprViewType, typename ExprTriggerType, typename Derived>
+struct ExprTriggerHelper
+    : public OpCatchUndef<ExprViewType>::ExprTriggerBase,
+      public ChangeTriggerAdapter<ExprTriggerType, Derived> {
+    using OpCatchUndef<ExprViewType>::ExprTriggerBase::ExprTriggerBase;
+};
+
+template <typename ExprViewType, typename Derived>
+struct ExprTriggerHelper<ExprViewType, TupleTrigger, Derived>
+    : public OpCatchUndef<ExprViewType>::ExprTriggerBase,
+      public ChangeTriggerAdapter<TupleTrigger, Derived> {
+    using OpCatchUndef<ExprViewType>::ExprTriggerBase::ExprTriggerBase;
+    void memberHasBecomeDefined(UInt) final {
+        if (this->op->expr->view().numberUndefined == 0) {
+            static_cast<Derived&>(*this).adapterHasBecomeDefined();
+        }
+    }
+    void memberHasBecomeUndefined(UInt) final {
+        if (this->op->expr->view().numberUndefined == 1) {
+            static_cast<Derived&>(*this).adapterHasBecomeUndefined();
+        }
+    }
+};
+
+template <typename ExprViewType, typename TriggerType>
+struct ExprTrigger
+    : public ExprTriggerHelper<ExprViewType, TriggerType,
+                               ExprTrigger<ExprViewType, TriggerType>> {
+    typedef typename AssociatedViewType<TriggerType>::type ExprType;
+    using ExprTriggerHelper<
+        ExprViewType, TriggerType,
+        ExprTrigger<ExprViewType, TriggerType>>::ExprTriggerHelper;
+
+    void adapterPossibleValueChange() {}
+
+    void adapterValueChanged() {}
+
+    void adapterHasBecomeDefined() {
+        visitTriggers([&](auto& t) { t->possibleValueChange(); },
+                      this->op->triggers);
+        this->op->exprDefined = true;
+        visitTriggers(
+            [&](auto& t) {
+                t->valueChanged();
+                t->reattachTrigger();
+            },
+            this->op->triggers);
+    }
+
+    void adapterHasBecomeUndefined() {
+        visitTriggers([&](auto& t) { t->possibleValueChange(); },
+                      this->op->triggers);
+        this->op->exprDefined = false;
+        visitTriggers(
+            [&](auto& t) {
+                t->valueChanged();
+                t->reattachTrigger();
+            },
+            this->op->triggers);
+    }
+    void reattachTrigger() final {
+        deleteTrigger(
+            static_pointer_cast<ExprTrigger<ExprViewType, TriggerType>>(
+                this->op->exprTrigger));
+        auto trigger =
+            make_shared<ExprTrigger<ExprViewType, TriggerType>>(this->op);
+        this->op->expr->addTrigger(trigger);
+        this->op->exprTrigger = trigger;
+    }
+};
+}  // namespace
+
+template <typename ExprViewType>
+void OpCatchUndef<ExprViewType>::startTriggeringImpl() {
+    if (!exprTrigger) {
+        typedef typename AssociatedTriggerType<ExprViewType>::type TriggerType;
+        auto trigger =
+            make_shared<ExprTrigger<ExprViewType, TriggerType>>(this);
+        exprTrigger = trigger;
+        expr->addTrigger(trigger);
+        expr->startTriggering();
+    }
+}
+
+template <typename ExprViewType>
+void OpCatchUndef<ExprViewType>::stopTriggeringOnChildren() {
+    if (exprTrigger) {
+        deleteTrigger(
+            static_pointer_cast<ExprTrigger<ExprViewType, ExprTriggerType>>(
+                exprTrigger));
+        exprTrigger = nullptr;
+    }
+}
+
+template <typename ExprViewType>
+void OpCatchUndef<ExprViewType>::stopTriggering() {
+    if (exprTrigger) {
+        stopTriggeringOnChildren();
+        expr->stopTriggering();
+    }
+}
+
+template <typename ExprViewType>
+void OpCatchUndef<ExprViewType>::updateVarViolations(
+    const ViolationContext& vioContext, ViolationContainer& vioDesc) {
+    if (exprDefined) {
+        expr->updateVarViolations(vioContext, vioDesc);
+    }
+}
+
+template <typename ExprViewType>
+ExprRef<ExprViewType> OpCatchUndef<ExprViewType>::deepCopySelfForUnrollImpl(
+    const ExprRef<ExprViewType>&, const AnyIterRef& iterator) const {
+    auto newOpCatchUndef = make_shared<OpCatchUndef<ExprViewType>>(
+        expr->deepCopySelfForUnroll(expr, iterator), replacement);
+    newOpCatchUndef->exprDefined = exprDefined;
+    return newOpCatchUndef;
+}
+
+template <typename ExprViewType>
+std::ostream& OpCatchUndef<ExprViewType>::dumpState(std::ostream& os) const {
+    os << "opCatchUndef(exprDefined=" << exprDefined << ",\nexpr=";
+    expr->dumpState(os) << ",\n";
+    os << "replacement=";
+    replacement->dumpState(os) << ")";
+    return os;
+}
+
+template <typename ExprViewType>
+void OpCatchUndef<ExprViewType>::findAndReplaceSelf(
+    const FindAndReplaceFunction& func) {
+    this->expr = findAndReplace(expr, func);
+    this->replacement = findAndReplace(replacement, func);
+}
+
+template <typename ExprViewType>
+bool OpCatchUndef<ExprViewType>::isUndefined() {
+    return false;
+}
+
+template <typename ExprViewType>
+pair<bool, ExprRef<ExprViewType>> OpCatchUndef<ExprViewType>::optimise(
+    PathExtension path) {
+    bool changeMade = false;
+    auto optResult = expr->optimise(path.extend(expr));
+    changeMade |= optResult.first;
+    expr = optResult.second;
+    optResult = replacement->optimise(path.extend(replacement));
+    changeMade |= optResult.first;
+    replacement = optResult.second;
+    return make_pair(changeMade, mpark::get<ExprRef<ExprViewType>>(path.expr));
+}
+
+template <typename Op>
+struct OpMaker;
+
+template <typename View>
+struct OpMaker<OpCatchUndef<View>> {
+    static ExprRef<View> make(ExprRef<View> expr, ExprRef<View> replacement);
+};
+
+template <typename View>
+ExprRef<View> OpMaker<OpCatchUndef<View>>::make(ExprRef<View> expr,
+                                                ExprRef<View> replacement) {
+    return make_shared<OpCatchUndef<View>>(move(expr), move(replacement));
+}
+
+#define opCatchUndefInstantiators(name)       \
+    template struct OpCatchUndef<name##View>; \
+    template struct OpMaker<OpCatchUndef<name##View>>;
+
+buildForAllTypes(opCatchUndefInstantiators, );
+#undef opCatchUndefInstantiators
---
+++ src/operators/opCatchUndef.h
@@ -0,0 +1,51 @@
+
+#ifndef SRC_OPERATORS_OPCATCHUNDEF_H_
+#define SRC_OPERATORS_OPCATCHUNDEF_H_
+
+#include "base/base.h"
+
+template <typename ExprViewType>
+struct OpCatchUndef : public ExprInterface<ExprViewType> {
+    struct ExprTriggerBase {
+        OpCatchUndef<ExprViewType>* op;
+        ExprTriggerBase(OpCatchUndef<ExprViewType>* op) : op(op) {}
+        virtual ~ExprTriggerBase() {}
+    };
+
+    typedef typename AssociatedTriggerType<ExprViewType>::type ExprTriggerType;
+    std::vector<std::shared_ptr<TriggerBase>> triggers;
+    ExprRef<ExprViewType> expr;
+    ExprRef<ExprViewType> replacement;
+    bool exprDefined = false;
+    std::shared_ptr<ExprTriggerBase> exprTrigger;
+
+    OpCatchUndef(ExprRef<ExprViewType> expr, ExprRef<ExprViewType> replacement)
+        : expr(std::move(expr)), replacement(std::move(replacement)) {}
+    OpCatchUndef(const OpCatchUndef<ExprViewType>&) = delete;
+    OpCatchUndef(OpCatchUndef<ExprViewType>&& other);
+    ~OpCatchUndef() { this->stopTriggeringOnChildren(); }
+    void addTriggerImpl(const std::shared_ptr<ExprTriggerType>& trigger,
+                        bool includeMembers, Int memberIndex) final;
+    ExprViewType& view() final;
+    const ExprViewType& view() const final;
+
+    void evaluateImpl() final;
+    void startTriggeringImpl() final;
+    void stopTriggering() final;
+    void stopTriggeringOnChildren();
+
+    void updateVarViolations(const ViolationContext& vioContext,
+                             ViolationContainer&) final;
+    ExprRef<ExprViewType> deepCopySelfForUnrollImpl(
+        const ExprRef<ExprViewType>& self,
+        const AnyIterRef& iterator) const final;
+    std::ostream& dumpState(std::ostream& os) const final;
+    void findAndReplaceSelf(const FindAndReplaceFunction&) final;
+    bool isUndefined();
+    ExprRef<ExprViewType>& getMember();
+    const ExprRef<ExprViewType>& getMember() const;
+    void reevaluate();
+    std::pair<bool, ExprRef<ExprViewType>> optimise(PathExtension path) final;
+};
+
+#endif /* SRC_OPERATORS_OPCATCHUNDEF_H_ */

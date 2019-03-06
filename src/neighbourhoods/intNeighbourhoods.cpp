ec89264d3b3f0cf185f87aec29c3b21b6c87b576
saad_attieh <saad_attieh@me.com>
2019-03-06 15:20:44 +0000

    Int assign random neighbourhood is now a struct

--- src/neighbourhoods/intNeighbourhoods.cpp
+++ src/neighbourhoods/intNeighbourhoods.cpp
@@ -32,45 +32,50 @@ void assignRandomValueInDomain<IntDomain>(const IntDomain& domain,
     });
 }
 
+struct IntAssignRandom {
+    const IntDomain& domain;
+    IntAssignRandom(const IntDomain& domain) : domain(domain) {}
+
+    void operator()(NeighbourhoodParams& params) {
+        auto& val = *(params.getVals<IntValue>().front());
+        int numberTries = 0;
+        const int tryLimit = params.parentCheckTryLimit;
+        debug_neighbourhood_action("Assigning random value: original value is "
+                                   << asView(val));
+        auto backup = val.value;
+        bool success;
+        do {
+            success = val.changeValue([&]() {
+                val.value = getRandomValueInDomain(domain, params.stats);
+                if (params.parentCheck(params.vals)) {
+                    return true;
+                } else {
+                    val.value = backup;
+                    return false;
+                }
+            });
+            if (success) {
+                debug_neighbourhood_action("New value is " << asView(val));
+            }
+        } while (!success && ++numberTries < tryLimit);
+        if (!success) {
+            debug_neighbourhood_action(
+                "Couldn't find value, number tries=" << tryLimit);
+            return;
+        }
+        if (!params.changeAccepted()) {
+            debug_neighbourhood_action("Change rejected");
+            val.changeValue([&]() {
+                val.value = backup;
+                return true;
+            });
+        }
+    }
+};
+
 void intAssignRandomGen(const IntDomain& domain, int numberValsRequired,
                         std::vector<Neighbourhood>& neighbourhoods) {
-    neighbourhoods.emplace_back(
-        "intAssignRandom", numberValsRequired,
-        [&domain](NeighbourhoodParams& params) {
-            auto& val = *(params.getVals<IntValue>().front());
-            int numberTries = 0;
-            const int tryLimit = params.parentCheckTryLimit;
-            debug_neighbourhood_action(
-                "Assigning random value: original value is " << asView(val));
-            auto backup = val.value;
-            bool success;
-            do {
-                success = val.changeValue([&]() {
-                    val.value = getRandomValueInDomain(domain, params.stats);
-                    if (params.parentCheck(params.vals)) {
-                        return true;
-                    } else {
-                        val.value = backup;
-                        return false;
-                    }
-                });
-                if (success) {
-                    debug_neighbourhood_action("New value is " << asView(val));
-                }
-            } while (!success && ++numberTries < tryLimit);
-            if (!success) {
-                debug_neighbourhood_action(
-                    "Couldn't find value, number tries=" << tryLimit);
-                return;
-            }
-            if (!params.changeAccepted()) {
-                debug_neighbourhood_action("Change rejected");
-                val.changeValue([&]() {
-                    val.value = backup;
-                    return true;
-                });
-            }
-        });
+    neighbourhoods.emplace_back("intAssignRandom", numberValsRequired, IntAssignRandom(domain));
 }
 
 const NeighbourhoodVec<IntDomain> NeighbourhoodGenList<IntDomain>::value = {

e70645d91c43f1c39e9f6e522da3ada0b1346910
Chris Jefferson <caj21@st-andrews.ac.uk>
2019-07-18 14:14:34 +0100

    Add missing headers for linux

--- src/utils/filename.h
+++ src/utils/filename.h
@@ -1,6 +1,9 @@
 
 #ifndef SRC_UTILS_FILENAME_H_
 #define SRC_UTILS_FILENAME_H_
+
+#include <string.h>
+
 #define __FILENAME__ \
     (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
 #endif /* SRC_UTILS_FILENAME_H_ */

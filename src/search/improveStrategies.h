bf05129d054a0fc849896a71a15a224b7682f0fd
saad_attieh <saad_attieh@me.com>
2020-04-06 11:23:35 +0300

    changed LAHC to always add to queue.

--- src/search/improveStrategies.h
+++ src/search/improveStrategies.h
@@ -112,61 +112,52 @@ class LateAcceptanceHillClimbing : public SearchStrategy {
         Objective bestObjective = state.model.getObjective();
         UInt bestViolation = state.model.getViolation();
         while (true) {
-            bool allowed = false, improvesOnBest = false;
-
+            bool wasViolating = state.model.getViolation();
             searcher->search(
                 state, selector->nextNeighbourhood(state),
                 [&](const auto& result) {
+                    bool allowed = false;
                     if (result.foundAssignment) {
                         if (result.statsMarkPoint.lastViolation != 0) {
                             allowed = result.model.getViolation() <=
                                           vioHistory.front() ||
                                       result.model.getViolation() <=
                                           vioHistory.back();
-                            improvesOnBest =
-                                result.model.getViolation() < bestViolation;
-                            if (allowed) {
-                                addToQueue(vioHistory,
-                                           state.model.getViolation());
-                            }
-
-                            if (improvesOnBest) {
-                                bestViolation = result.model.getViolation();
-                            }
-                            if (result.statsMarkPoint.lastViolation != 0 &&
-                                result.model.getViolation() == 0) {
-                                objHistory = {state.model.getObjective()};
-                            }
                         } else if (result.model.getViolation() == 0) {
-                            // last violation was 0, current violation is 0,
+                    // last violation was 0, current violation is 0,
                             // check objective:
                             allowed = result.model.getObjective() <=
                                           objHistory.front() ||
                                       result.model.getObjective() <=
                                           objHistory.back();
-                            improvesOnBest =
-                                result.model.getObjective() < bestObjective;
-                            if (allowed) {
-                                addToQueue(objHistory,
-                                           state.model.getObjective());
-                            }
-
-                            if (improvesOnBest) {
-                                bestObjective = result.model.getObjective();
-                            }
                         }
                     }
                     return allowed;
                 });
-
-            if (isOuterMostStrategy) {
-                continue;
+            bool isViolating = state.model.getViolation() > 0;
+            bool improvesOnBest = false;
+            if (wasViolating && isViolating) {
+                UInt violation = state.model.getViolation();
+                addToQueue(vioHistory, violation);
+                improvesOnBest = violation < bestViolation;
+            } else if (wasViolating && !isViolating) {
+                vioHistory.clear();
+                objHistory.clear();
+                addToQueue(objHistory, state.model.getObjective());
+                improvesOnBest = true;
+            } else if (!wasViolating && !isViolating) {
+                auto obj = state.model.getObjective();
+                addToQueue(objHistory, obj);
+                improvesOnBest = obj < bestObjective;
             }
             if (improvesOnBest) {
+                bestViolation = state.model.getViolation();
+                bestObjective = state.model.getObjective();
                 iterationsAtPeak = 0;
             } else {
                 ++iterationsAtPeak;
-                if (iterationsAtPeak > allowedIterationsAtPeak) {
+                if (!isOuterMostStrategy &&
+                    iterationsAtPeak > allowedIterationsAtPeak) {
                     break;
                 }
             }
